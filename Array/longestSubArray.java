bdbb83a29b11fb99caf2b4736eb6e0f83d5f5472
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-08 13:48:04 +0530

    Longgest subarray Optimized approach

--- Array/longestSubArray.java
+++ Array/longestSubArray.java
@@ -1,8 +1,10 @@
 public class longestSubArray {
     public static void main(String[] args) {
-        int arr[]={1,2,3,1,1,1,1,4,2,3};
+        int arr[]={2,0,0,0,3};
         int k=3;
         System.out.println(findLongestSubArray(arr, k));
+        System.out.println(longestSubArrayBetter(arr, k));
+        System.out.println(longestSubArrayOptimized(arr, k));
     }
     //Brute force O(N*N*N)
     public static int findLongestSubArray(int[]arr,int sum){
@@ -19,6 +21,42 @@ public class longestSubArray {
             }
         }
         return len;
+    }
+    //Better O(N*N);
+    public static int longestSubArrayBetter(int []arr,int sum){
+        int len=0;
+        for(int i=0;i<arr.length;i++){
+            int s=0;
+            for(int j=i;j<arr.length;j++){
+                s+=arr[j];
+                if(s==sum){
+                    len=Math.max(len, j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+
+    //Optimized
+    public static int longestSubArrayOptimized(int []arr,int s){
+        int left=0,right=0,maxLen=0;
+        int sum=arr[0];
+        while (right<arr.length) {
+            while (left<=right && sum > s) {
+                sum-=arr[left];
+                left++;
+                
+            }
+            if(sum==s){
+                maxLen=Math.max(maxLen, right-left+1);
+            }
+            right++;
+            if(right<arr.length){
+                sum+=arr[right];
+            }
+            
+        }
+        return maxLen;
     }
     
 }
