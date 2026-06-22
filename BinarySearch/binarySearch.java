4ad09fbd116fbdeebc9e7c39c0e3ca3649ca3bfa
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-22 08:05:42 +0530

    Binary Search using recursion

--- BinarySearch/binarySearch.java
+++ BinarySearch/binarySearch.java
@@ -1,7 +1,8 @@
 public class binarySearch{
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6};
-        System.out.println(bs(arr, 2));
+        //System.out.println(bs(arr, 2));
+        System.out.println(bsRecursion(arr, 0, arr.length-1, 7));
         
     }
     public static int bs(int []arr,int target){
@@ -13,5 +14,18 @@ public class binarySearch{
             else low=mid+1;
         }
         return -1;
+    }
+    //Using recursion
+    public static int bsRecursion(int[]arr,int low,int high,int target){
+        if(low>high){
+            return -1;
+        }
+        int mid=(low+high)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]>target){
+          return  bsRecursion(arr, low, mid-1, target);
+        }else{
+           return bsRecursion(arr, mid+1, high, target);
+        }
     }
 }
