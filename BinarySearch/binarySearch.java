be2be992a792b4272e820ce7043054e06637ecde
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-22 07:50:39 +0530

    Binary Search

---
+++ BinarySearch/binarySearch.java
@@ -0,0 +1,17 @@
+public class binarySearch{
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        System.out.println(bs(arr, 2));
+        
+    }
+    public static int bs(int []arr,int target){
+        int low=0,high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
+    }
+}
