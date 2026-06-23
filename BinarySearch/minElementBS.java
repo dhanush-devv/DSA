b9da318a90bc95c73d67c0bf003eba37e7b1d6c1
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-23 11:35:09 +0530

    Find min element in rotated array using BS

---
+++ BinarySearch/minElementBS.java
@@ -0,0 +1,21 @@
+public class minElementBS {
+    public static void main(String[] args) {
+        int arr[]={4,5,6,7,0,1,2,3};
+        System.out.println(findMin(arr));
+        
+    }
+    public static int findMin(int []arr){
+        int low=0, high=arr.length-1,ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[low]<=arr[mid]){
+                ans=Math.min(ans,arr[low]);
+                low=mid+1;
+            }else{
+                ans=Math.min(ans,arr[mid]);
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
