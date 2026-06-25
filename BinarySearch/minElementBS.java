7094ec390178aa93ffb477c3e5087e5d164eb56b
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-25 11:17:12 +0530

    Rotation Postitin

--- BinarySearch/minElementBS.java
+++ BinarySearch/minElementBS.java
@@ -8,6 +8,10 @@ public class minElementBS {
         int low=0, high=arr.length-1,ans=Integer.MAX_VALUE;
         while(low<=high){
             int mid=(low+high)/2;
+            if(arr[low]<=arr[high]){
+                ans=Math.min(ans,arr[low]);
+                break;
+            }
             if(arr[low]<=arr[mid]){
                 ans=Math.min(ans,arr[low]);
                 low=mid+1;
